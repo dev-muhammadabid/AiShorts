9887937fda7eec548788cc3424fa280c72973920
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-12-20 01:09:23 +0530

    Working On Integrate The Gemini

--- src/main/java/dev/abid/AiShorts/controllers/AiShortsController.java
+++ src/main/java/dev/abid/AiShorts/controllers/AiShortsController.java
@@ -1,4 +1,26 @@
 package dev.abid.AiShorts.controllers;
 
+import dev.abid.AiShorts.services.AiShortsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/qna")
 public class AiShortsController {
+
+    private final AiShortsService aiShortsService;
+
+    @PostMapping("/ask")
+    public ResponseEntity<String> askQuestion(@RequestBody Map<String, String> payload){
+        String question = payload.get("question");
+        String answer = aiShortsService.getAnswer(question);
+        return ResponseEntity.ok(answer);
+    }
 }
--- src/main/java/dev/abid/AiShorts/services/AiShortsService.java
+++ src/main/java/dev/abid/AiShorts/services/AiShortsService.java
@@ -1,4 +1,44 @@
 package dev.abid.AiShorts.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+
+@Service
 public class AiShortsService {
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    private final WebClient webClient;
+
+    public AiShortsService(WebClient.Builder webClient){
+        this.webClient = webClient.build();
+    }
+
+    public String getAnswer (String question){
+
+        Map<String, Object> requestBody = Map.of("content", new Object[]{
+                        Map.of("parts", new Object[] {
+                                Map.of("text", question)
+                        })
+                }
+        );
+
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return response;
+    }
 }
