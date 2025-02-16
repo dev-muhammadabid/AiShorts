ad9d2f719b4dec6eefd14d840fc3d8d2c09b8fe2
dev-muhammadabid <muhammadabid.dev@gmail.com>
2025-02-16 21:39:05 +0530

    first commit

---
+++ aishorts-backend/src/test/java/dev/abid/aishorts_backend/AishortsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abid.aishorts_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AishortsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
